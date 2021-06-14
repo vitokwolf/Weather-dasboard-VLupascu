4f7c3861298b6377c2ed037ffe9b83801653bd3e
Victor Lupascu <sinajeen@gmail.com>
2021-06-13 22:12:21 -0500

    add search input functionality, display searched city and dynamically create a button, saving to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 var cityInputField = document.getElementById('city-input');
+var previousSearches = document.getElementById('history');
+
 var cityDisplayed = document.getElementById('current-city-displayed');
 var searchField = document.getElementById('search-form');
 var currentCityTemp = document.getElementById('current-city-temp');
@@ -8,6 +10,34 @@ var currentCityUV = document.getElementById('current-city-uv');
 var currentCityIcon = document.getElementById('weather-icon');
 var citiesArray = JSON.parse(localStorage.getItem('cities')) || [];
 var apiKey = 'de36f3d17ff255fa1488513e597dbf11';
+
+// Start search city
+function formSubmitHandler(event) {
+    event.preventDefault();
+    var currentCity = cityInputField
+        .value
+        .trim();
+        console.log(currentCity);
+// check if searched city was searched before
+    if (citiesArray.indexOf(currentCity) === -1) {
+        // if not present in the localStorage push in the array and save
+        citiesArray.push(currentCity);
+        cityDisplayed.innerHTML = currentCity;
+        localStorage.setItem("cities", JSON.stringify(citiesArray));
+        // create a new button for the searched city
+        var newBtn = document.createElement("button");    
+        newBtn.classList = "btn btn-outline-primary btn-lg btn-block city-btn";
+        newBtn.setAttribute("id", "city-" + currentCity)
+        newBtn.innerHTML = currentCity;
+        // append it to history container
+        previousSearches.appendChild(newBtn);
+    };
+    // clear search form
+    cityInputField.value = "";
+};
+// call function on click
+searchField.addEventListener("submit", formSubmitHandler);
+// End search city
 
 // Start create dates and times across the page
 function displayDate() {
